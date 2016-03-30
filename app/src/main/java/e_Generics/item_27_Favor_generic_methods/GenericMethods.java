cbcc6c73976e225986d67b1811864a775000b9a5
Egis <e.gecius@gmail.com>
2016-03-30 08:04:04 +0100

    more sample code for raw types

--- app/src/main/java/e_Generics/item_27_Favor_generic_methods/GenericMethods.java
+++ app/src/main/java/e_Generics/item_27_Favor_generic_methods/GenericMethods.java
@@ -5,8 +5,16 @@ import java.util.Set;
 
 public class GenericMethods {
 
-	public <E> Set<E> union(Set<E> s1, Set<? extends E> s2) {
+	/** Generic - can accept of type or their children */
+	public <E> Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
 		Set<E> result = new HashSet<>(s1);
+		result.addAll(s2);
+		return result;
+	}
+
+	/** This method has no choice bu to return a Set of raw type, accepting any object - unacceptable. */
+	public Set union_with_raw_types(Set s1, Set s2) {
+		Set result = new HashSet<>(s1);
 		result.addAll(s2);
 		return result;
 	}
