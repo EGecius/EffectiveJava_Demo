6e9346bdba609f9565a9d55c8e1e82a607771eee
Egis <e.gecius@gmail.com>
2016-04-01 07:12:47 +0100

    Recursive type bound

--- app/src/main/java/e_Generics/item_27_Favor_generic_methods/RecursiveTypeBound.java
+++ app/src/main/java/e_Generics/item_27_Favor_generic_methods/RecursiveTypeBound.java
@@ -1,7 +1,27 @@
 package e_Generics.item_27_Favor_generic_methods;
 
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * //todo
  */
 final class RecursiveTypeBound {
+
+	public <T extends Comparable<T>> T max(List<T> list) {
+
+		Iterator<T> iterator = list.iterator();
+		T result = iterator.next();
+
+		while (iterator.hasNext()) {
+
+			T next = iterator.next();
+
+			if (next.compareTo(result) > 0) {
+				result = next;
+			}
+		}
+
+		return result;
+	}
 }
---
+++ app/src/test/java/e_Generics/item_27_Favor_generic_methods/RecursiveTypeBoundTest.java
@@ -0,0 +1,51 @@
+package e_Generics.item_27_Favor_generic_methods;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link RecursiveTypeBound}
+ */
+public class RecursiveTypeBoundTest {
+
+	RecursiveTypeBound sample;
+
+	@Before
+	public void setup() {
+		sample = new RecursiveTypeBound();
+	}
+
+	@Test
+	public void when_Integers() {
+
+		ArrayList<Integer> integers = new ArrayList<>();
+		integers.add(10);
+		integers.add(5);
+		integers.add(7);
+
+		//WHEN
+		Integer max = sample.max(integers);
+
+		//THEN
+		assertThat(max).isEqualTo(10);
+	}
+
+	@Test
+	public void when_Characters() {
+		//WHEN
+		ArrayList<String> characters = new ArrayList<>();
+
+		//THEN
+		characters.add("z");
+		characters.add("a");
+		characters.add("m");
+		String max = sample.max(characters);
+		assertThat(max).isEqualTo("z");
+	}
+
+
+}
