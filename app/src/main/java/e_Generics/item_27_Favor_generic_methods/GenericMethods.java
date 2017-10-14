8d4433d061b06f3c83e82e013154d79af5c371b2
Egidijus Gecius <e.gecius@gmail.com>
2017-10-14 07:10:24 +0100

    clarify use of generic methods

--- app/src/main/java/e_Generics/item_27_Favor_generic_methods/GenericMethods.java
+++ app/src/main/java/e_Generics/item_27_Favor_generic_methods/GenericMethods.java
@@ -8,14 +8,25 @@ public class GenericMethods {
 	/** Generic - can accept of type or their children */
 	public <E> Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
 		Set<E> result = new HashSet<>(s1);
+
+        //adding same type compiles!
 		result.addAll(s2);
+
+        // TYPE SAFETY - adding different type does not compile
+//        result.add(new String());
+
 		return result;
 	}
 
-	/** This method has no choice bu to return a Set of raw type, accepting any object - unacceptable. */
+	/** This method has no choice but to return a Set of raw type, accepting any object -
+     * unacceptable. */
 	public Set union_with_raw_types(Set s1, Set s2) {
 		Set result = new HashSet<>(s1);
 		result.addAll(s2);
+
+        // NO TYPE SAFETY - here you can add any type
+        result.add(new String());
+
 		return result;
 	}
 
